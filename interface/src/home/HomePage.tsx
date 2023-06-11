3d00c72a125ff76430ce819636c55d2091a345db
Junho Yeo <i@junho.io>
2023-06-12 01:41:03 +0900

    [interface] Initialize project

---
+++ interface/src/home/HomePage.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+const HomePage = () => {
+  return <Container />;
+};
+
+export default HomePage;
+
+const Container = styled.div``;
