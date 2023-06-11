29d62809f36766359fc9e886a1a56e5ce55ea52e
Junho Yeo <i@junho.io>
2023-06-11 14:53:29 +0900

    🌈 Format files

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 <img src=".github/logo.png" width="400" />
 
-# CryptoGPT
-- ✸ 🏦 **Allowing *Financial Autonomy* to *[Sam's Children](https://en.wikipedia.org/wiki/ChatGPT)***
+<h1>
+  ✸ CryptoGPT — <br />
+  An experiment for <code>🤖 LLMs</code> <br />
+  archiving <code>🏦 Financial Autonomy</code>
+</h1>
+
+- ✸ 🏦 **Allowing _Financial Autonomy_ to _[Sam's Children](https://en.wikipedia.org/wiki/ChatGPT)_**
 - ✸ 🪪 Enabling Language Learning Models (LLMs) to **establish their own unique identities** within the blockchain.
-- ✸ 🇦🇶 **Make the name *CryptoGPT* great again.** Not affiliated with [LayerAI](https://layerai.org/) or any other (scammy) web3 projects.
+- ✸ 🇦🇶 **Make the name _CryptoGPT_ great again.** Not affiliated with [LayerAI](https://layerai.org/) or any other (scammy) web3 projects.
--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,54 @@
-import { OpenAI } from "langchain/llms/openai";
-import { initializeAgentExecutorWithOptions } from "langchain/agents";
-import { DynamicStructuredTool, DynamicTool } from "langchain/tools";
-import  {  JsonRpcProvider, Wallet } from 'ethers'
+import { JsonRpcProvider, Wallet } from 'ethers';
+import { initializeAgentExecutorWithOptions } from 'langchain/agents';
+import { OpenAI } from 'langchain/llms/openai';
+import { InMemoryFileStore } from 'langchain/stores/file/in_memory';
+import { DynamicStructuredTool, DynamicTool } from 'langchain/tools';
+import { z } from 'zod';
 
-import { InMemoryFileStore } from "langchain/stores/file/in_memory";
-import { z } from "zod";
-
-require('dotenv').config()
+require('dotenv').config();
 
 const store = new InMemoryFileStore();
 
 export const run = async () => {
   // const vectorStore = new MemoryVectorStore(new OpenAIEmbeddings());
   const model = new OpenAI({
-    temperature: 0, openAIApiKey : process.env.OPENAI_API_KEY });
+    temperature: 0,
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
 
   // random wallet
-  const provider = new JsonRpcProvider('https://public-en-baobab.klaytn.net')
+  const provider = new JsonRpcProvider('https://public-en-baobab.klaytn.net');
   // const wallet = Wallet.createRandom()
-  const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY || '')
-  const signer = wallet.connect(provider)
-  console.log(wallet.address)
+  const wallet = new Wallet(process.env.WALLET_PRIVATE_KEY || '');
+  const signer = wallet.connect(provider);
+  console.log(wallet.address);
   // console.log(wallet.privateKey)
 
   const tools = [
     new DynamicTool({
-      name: "eth.address",
-      description:
-        "call this to get your wallet address",
-      func: async () =>  wallet.address
+      name: 'eth.address',
+      description: 'call this to get your wallet address',
+      func: async () => wallet.address,
     }),
     new DynamicTool({
-      name: "eth.balance",
-      description: "call this to get wallet balance of any input address",
+      name: 'eth.balance',
+      description: 'call this to get wallet balance of any input address',
       func: async (address) => {
         try {
-        const balance = await provider.getBalance(address)
-        console.log({ balance })
-        return balance.toString()
+          const balance = await provider.getBalance(address);
+          console.log({ balance });
+          return balance.toString();
         } catch (e) {
-          console.log(e)
-          return '0'
+          console.log(e);
+          return '0';
         }
       },
     }),
     // send with params as schema
     new DynamicStructuredTool({
-      name: "eth.send",
-      description: "call this to send transactions with your wallet. props are {to,value,nonce,gasLimit,gasPrice,data,chainId}. Leave values blank if you dont need them(ex> you dont usually add nonce). This tool returns transaction hash after executing your transaction.",
+      name: 'eth.send',
+      description:
+        'call this to send transactions with your wallet. props are {to,value,nonce,gasLimit,gasPrice,data,chainId}. Leave values blank if you dont need them(ex> you dont usually add nonce). This tool returns transaction hash after executing your transaction.',
       schema: z.object({
         to: z.string(),
         value: z.string().or(z.number()),
@@ -58,15 +59,15 @@ export const run = async () => {
         chainId: z.string().or(z.number()),
       }),
       func: async (params) => {
-        const cleaned = Object.fromEntries(Object.entries(params).filter(([_, v]) => v !== ''))
-        const tx = await signer.sendTransaction(cleaned)
-        return tx.hash
+        const cleaned = Object.fromEntries(Object.entries(params).filter(([_, v]) => v !== ''));
+        const tx = await signer.sendTransaction(cleaned);
+        return tx.hash;
       },
     }),
   ];
 
   const executor = await initializeAgentExecutorWithOptions(tools, model, {
-    agentType: "structured-chat-zero-shot-react-description",
+    agentType: 'structured-chat-zero-shot-react-description',
     verbose: true,
     returnIntermediateSteps: true,
     // memory: new BufferMemory({
@@ -75,30 +76,30 @@ export const run = async () => {
     // }),
     agentArgs: {
       // inputVariables: ["input", "agent_scratchpad", "chat_history"],
-      inputVariables: ["input", "agent_scratchpad", ],
+      inputVariables: ['input', 'agent_scratchpad'],
       // memoryPrompts: [new MessagesPlaceholder("chat_history")],
     },
-      // memory: vectorStore.asRetriever(),
+    // memory: vectorStore.asRetriever(),
   });
 
-  console.log("Loaded agent.");
+  console.log('Loaded agent.');
 
   const input = `Send 0 eth to your wallet address using your wallet, and return the transaction hash(hint: you might want to get your address first using eth.address tool).`;
 
   console.log(`Executing with input "${input}"...`);
 
   try {
-  const result = await executor.call({ input,  });
+    const result = await executor.call({ input });
 
-  console.log(`Got output ${result.output}`);
+    console.log(`Got output ${result.output}`);
 
-  // // send 0 eth transaction to yourself
-  // const input2 = `Send 0 eth to yourself`;
+    // // send 0 eth transaction to yourself
+    // const input2 = `Send 0 eth to yourself`;
 
-  // console.log(`Executing with input "${input2}"...`);
-  // const result2 = await executor.call({ input: input2,  });
+    // console.log(`Executing with input "${input2}"...`);
+    // const result2 = await executor.call({ input: input2,  });
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 };
-run()
+run();
