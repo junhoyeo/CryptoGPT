7d57f9ea6cfd1dd09e19ddb8dea35abb971a4996
Junho Yeo <i@junho.io>
2023-06-15 17:53:11 +0900

    [interface] Receive goal from user input

--- interface/src/home/HomePage.tsx
+++ interface/src/home/HomePage.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 import { Config } from '@junhoyeo/cryptogpt';
-import { CheckCircle, Zap } from 'lucide-react';
+import { Box, CheckCircle, Zap } from 'lucide-react';
 import getNextConfig from 'next/config';
 import React, { useCallback, useState } from 'react';
 import { useLocalStorage } from '@/hooks/useLocalStorage';
@@ -33,11 +33,10 @@ type AgentEvent = {
 
 const { publicRuntimeConfig } = getNextConfig();
 
-const goals = [
-  `Retrieve your wallet address.`,
-  `Send zero value transaction to yourself.`,
-  `Check transaction receipt.`,
-];
+const cleanObject = (params: object) =>
+  Object.fromEntries(Object.entries(params).filter(([_, v]) => v !== ''));
+
+const FIRST_GOAL = `Retrieve your wallet address.`;
 
 const HomePage = () => {
   const [loading, setLoading] = useState<boolean>(false);
@@ -47,6 +46,8 @@ const HomePage = () => {
     JSON_RPC_URL: publicRuntimeConfig.JSON_RPC_URL,
     WALLET_PRIVATE_KEY: publicRuntimeConfig.WALLET_PRIVATE_KEY,
   });
+
+  const [draft, setDraft] = useState<string>('');
 
   const onClickRun = useCallback(async () => {
     if (loading) {
@@ -59,7 +60,7 @@ const HomePage = () => {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ goals, config }),
+      body: JSON.stringify({ goals: [FIRST_GOAL, draft], config }),
     });
 
     if (!response.ok) {
@@ -91,26 +92,18 @@ const HomePage = () => {
     }
 
     setLoading(false);
-  }, []);
+  }, [draft]);
 
   console.log(events);
 
   return (
     <div className="w-full bg-slate-50">
-      <Container className="min-h-screen h-full container mx-auto max-w-xl bg-white pb-10">
-        {goals.map((goal) => (
-          <div key={goal}>{goal}</div>
-        ))}
-
-        <button disabled={loading} onClick={onClickRun}>
-          Run Agent
-        </button>
-
+      <Container className="min-h-screen h-full container mx-auto max-w-xl bg-white pt-5 pb-10">
         <div className="flex flex-col gap-3">
           {events.map((event, index) => (
             <div
               key={index}
-              className="flex flex-col bg-slate-100 w-fit max-w-[70%] py-3 px-4 rounded-xl rounded-tl-none"
+              className="flex flex-col bg-slate-100 w-fit max-w-[70%] py-3 pb-4 px-4 rounded-xl rounded-tl-none"
             >
               {event?.command?.name === 'finish' ? (
                 <span className="flex items-center gap-1 text-slate-700 text-xs">
@@ -122,16 +115,35 @@ const HomePage = () => {
                 </span>
               )}
 
-              <div className="mt-2">
+              <div className="flex flex-col mt-2 gap-2">
                 <p className="leading-snug text-sm">{event.thoughts.reasoning}</p>
                 {event?.command?.name !== 'finish' && (
-                  <div style={{ backgroundColor: 'greenyellow' }}>
-                    <span>{event.command.name}</span>
+                  <div className="bg-slate-300 p-2 rounded-md">
+                    <span className="flex items-center text-xs leading-none gap-1 text-slate-700">
+                      <Box size={14} /> <span className="font-medium">{event.command.name}</span>
+                    </span>
+                    {'args' in event.command && Object.keys(event.command.args).length > 0 && (
+                      <p className="text-xs break-all">{JSON.stringify(cleanObject(event.command.args))}</p>
+                    )}
                   </div>
                 )}
               </div>
             </div>
           ))}
+
+          <input
+            className="flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm placeholder:text-muted-foreground focus-visible:outline-none focus:bg-slate-50 disabled:cursor-not-allowed disabled:opacity-50"
+            value={draft}
+            onChange={(e) => setDraft(e.target.value)}
+          />
+
+          <button
+            className="px-4 py-2 rounded-lg bg-slate-700 text-sm text-slate-200 disabled:bg-slate-400"
+            disabled={loading}
+            onClick={onClickRun}
+          >
+            Run Agent
+          </button>
         </div>
       </Container>
     </div>
